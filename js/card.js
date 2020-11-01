b3962e53ca0d088149ad2f9998a392417e136bad
lattanya <latushevat@gmail.com>
2020-11-01 11:59:36 +0300

    исправления в названиях файлов

--- js/card.js
+++ js/card.js
@@ -1,282 +1,61 @@
 'use strict';
 
 (function () {
-  var getData = function () {
-    var dataArray = [
-      {
-        author: {
-          avatar: 'img/avatars/user01.png',
-        },
-        offer: {
-          title: 'Заголовок1',
-          address: '400, 350',
-          price: 5000,
-          type: 'palace',
-          rooms: 1,
-          guests: 1,
-          checkin: '14:00',
-          checkout: '14:00',
-          features: [
-            'wifi',
-            'dishwasher',
-            'parking',
-          ],
-          description: 'Все хорошо',
-          photos: [
-            'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-            'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-            'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
-          ],
-        },
+  var renderCard = function (obj) {
+    var template = document.querySelector('#card');
+    var content = template.content.querySelector('.map__card');
+    var card = content.cloneNode(true);
 
-        location: {
-          x: 100,
-          y: 400,
-        },
-      },
-      {
-        author: {
-          avatar: 'img/avatars/user02.png',
-        },
-        offer: {
-          title: 'Заголовок2',
-          address: '400, 350',
-          price: 15000,
-          type: 'flat',
-          rooms: 2,
-          guests: 3,
-          checkin: '14:00',
-          checkout: '14:00',
-          features: [
-            'wifi',
-            'dishwasher',
-            'parking',
-            'washer',
-            'elevator',
-            'conditioner',
-          ],
-          description: 'Все хорошо',
-          photos: [
-            'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-            'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-            'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
-          ],
-        },
+    var img = card.querySelector('img');
+    img.src = obj.author.avatar;
 
-        location: {
-          x: 300,
-          y: 500,
-        },
-      },
-      {
-        author: {
-          avatar: 'img/avatars/user03.png',
-        },
-        offer: {
-          title: 'Заголовок3',
-          address: '400, 350',
-          price: 5000,
-          type: 'flat',
-          rooms: 3,
-          guests: 10,
-          checkin: '14:00',
-          checkout: '14:00',
-          features: [
-            'wifi',
-            'dishwasher',
-            'parking',
-            'washer',
-            'elevator',
-            'conditioner',
-          ],
-          description: 'Все хорошо',
-          photos: [
-            'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-            'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-            'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
-          ],
-        },
+    var title = card.querySelector('.popup__title');
+    title.textContent = obj.offer.title;
 
-        location: {
-          x: 500,
-          y: 600,
-        },
-      },
-      {
-        author: {
-          avatar: 'img/avatars/user04.png',
-        },
-        offer: {
-          title: 'Заголовок4',
-          address: '400, 350',
-          price: 5000,
-          type: 'flat',
-          rooms: 1,
-          guests: 10,
-          checkin: '14:00',
-          checkout: '14:00',
-          features: [
-            'wifi',
-            'dishwasher',
-            'parking',
-            'washer',
-            'elevator',
-            'conditioner',
-          ],
-          description: 'Все хорошо',
-          photos: [
-            'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-            'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-            'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
-          ],
-        },
+    var address = card.querySelector('.popup__text--address');
+    address.textContent = obj.offer.address;
 
-        location: {
-          x: 150,
-          y: 360,
-        },
-      },
-      {
-        author: {
-          avatar: 'img/avatars/user05.png',
-        },
-        offer: {
-          title: 'Заголовок5',
-          address: '400, 350',
-          price: 5000,
-          type: 'palace',
-          rooms: 8,
-          guests: 10,
-          checkin: '14:00',
-          checkout: '14:00',
-          features: [
-            'wifi',
-            'dishwasher',
-            'parking',
-            'washer',
-            'elevator',
-            'conditioner',
-          ],
-          description: 'Все хорошо',
-          photos: [
-            'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-            'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-            'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
-          ],
-        },
+    var price = card.querySelector('.popup__text--price');
+    price.innerHTML = obj.offer.price + ' &#x20bd;/ночь';
 
-        location: {
-          x: 260,
-          y: 333,
-        },
-      },
-      {
-        author: {
-          avatar: 'img/avatars/user06.png',
-        },
-        offer: {
-          title: 'Заголовок6',
-          address: '400, 350',
-          price: 5000,
-          type: 'palace',
-          rooms: 8,
-          guests: 10,
-          checkin: '14:00',
-          checkout: '14:00',
-          features: [
-            'wifi',
-            'dishwasher',
-            'parking',
-            'washer',
-            'elevator',
-            'conditioner',
-          ],
-          description: 'Все хорошо',
-          photos: [
-            'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-            'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-            'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
-          ],
-        },
+    var type = card.querySelector('.popup__type');
+    type.textContent = window.HouseTypes[obj.offer.type];
 
-        location: {
-          x: 500,
-          y: 240,
-        },
-      },
-      {
-        author: {
-          avatar: 'img/avatars/user07.png',
-        },
-        offer: {
-          title: 'Заголовок7',
-          address: '400, 350',
-          price: 5000,
-          type: 'palace',
-          rooms: 8,
-          guests: 10,
-          checkin: '14:00',
-          checkout: '14:00',
-          features: [
-            'wifi',
-            'dishwasher',
-            'parking',
-            'washer',
-            'elevator',
-            'conditioner',
-          ],
-          description: 'Все хорошо',
-          photos: [
-            'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-            'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-            'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
-          ],
-        },
+    var capacity = card.querySelector('.popup__text--capacity');
+    capacity.textContent = obj.offer.rooms + ' комнаты для ' + obj.offer.guests + ' гостей';
 
-        location: {
-          x: 750,
-          y: 330,
-        },
-      },
-      {
-        author: {
-          avatar: 'img/avatars/user08.png',
-        },
-        offer: {
-          title: 'Заголовок8',
-          address: '400, 350',
-          price: 5000,
-          type: 'palace',
-          rooms: 8,
-          guests: 10,
-          checkin: '14:00',
-          checkout: '14:00',
-          features: [
-            'wifi',
-            'dishwasher',
-            'parking',
-            'washer',
-            'elevator',
-            'conditioner',
-          ],
-          description: 'Все хорошо',
-          photos: [
-            'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-            'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-            'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
-          ],
-        },
+    var time = card.querySelector('.popup__text--time');
+    time.textContent = 'Заезд после ' + obj.offer.checkin + ', выезд до ' + obj.offer.checkout;
 
-        location: {
-          x: 860,
-          y: 310,
-        },
-      },
-    ];
+    var features = card.querySelector('.popup__features');
+    features.innerHTML = '';
+    for (var i = 0; i < obj.offer.features.length; i++) {
+      var feature = obj.offer.features[i];
+      var item = document.createElement('li');
+      item.classList.add('popup__feature');
+      item.classList.add('popup__feature--' + feature);
+      features.appendChild(item);
+    }
 
-    return dataArray;
+    var description = card.querySelector('.popup__description');
+    description.textContent = obj.offer.description;
+
+    var photosWrapper = card.querySelector('.popup__photos');
+    var photo = photosWrapper.querySelector('.popup__photo');
+    photosWrapper.innerHTML = '';
+    for (var k = 0; k < obj.offer.photos.length; k++) {
+      var photoSrc = obj.offer.photos[k];
+      var imgCopy = photo.cloneNode();
+      imgCopy.src = photoSrc;
+      photosWrapper.appendChild(imgCopy);
+    }
+
+    card.classList.add('hidden');
+
+    return card;
   };
 
   // экспорт
-  window.getData = getData;
+  window.renderCard = renderCard;
+
 })();
